359803762fc21d4357be40fad0966816ed840fc6
pcprapolymers-boop <pcprapolymers@gmail.com>
2026-08-26 12:52:48 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,7 @@
  * Preencha com os dados da configuração Web App
  * do projeto Firebase usado para o monitoramento.
  */
-const RA_POLYMERS_FIREBASE_CONFIG = {
+window.RA_POLYMERS_FIREBASE_CONFIG = {
   apiKey: 'AIzaSyAN6r-yas54aDg1xxohhxxUqwaDoWZRAC4',
   authDomain: 'ra-polymers-monitoramento.firebaseapp.com',
   projectId: 'ra-polymers-monitoramento',
@@ -13,6 +13,6 @@ const RA_POLYMERS_FIREBASE_CONFIG = {
   appId: '1:598583808399:web:1f4613e6b144787a9d7bfa'
 };
 
-const RA_POLYMERS_VAPID_PUBLIC_KEY =
+window.RA_POLYMERS_VAPID_PUBLIC_KEY =
   'BEySqCF6-_yDloM1geweDVWBM47OMYqyUwqXvUzf1PuhpxYTlrSsNm9Rvw4CKHsOeqTGOr2k5G1csy0jRl9no-Q';
 
